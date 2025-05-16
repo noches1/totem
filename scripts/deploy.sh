9d3312f0f6bc0815e3dcd52f85e5a773bbb6c152
Ben Soh <ben.soh@discordapp.com>
2025-05-15 22:03:47 -0700

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,14 +9,14 @@ echo "Connected!"
 echo "Building client..."
 pnpm --offline --no-update-notifier --filter client build
 if [ $? -ne 0 ]; then
-    echo "Error: TypeScript type checking failed. Aborting deployment."
-    exit 1
+        echo "Error: TypeScript type checking failed. Aborting deployment."
+        exit 1
 fi
 echo "Client built!"
 
 echo "Zipping archive..."
 rm -f totem.zip
-if [ $1 == "fast" ]; then
+if [[ $1 == "fast" ]]; then
         zip -r totem.zip . -x "*.git*" -x "*.DS_Store*" -x "*.pnpm*" -x "*node_modules*" -x "images/*" -x "fonts/*" -x "*.venv*" -x "*__pycache__*"
 else
         zip -r totem.zip . -x "*.git*" -x "*.DS_Store*" -x "*.pnpm*" -x "*node_modules*" -x "*.venv*" -x "*__pycache__*"
@@ -27,7 +27,7 @@ echo "Uploading zip archive..."
 scp -i ./scripts/deploy_key totem.zip totem@totem.local:~
 
 echo "SSH into totem and extracting archive..."
-if [ $1 == "fast" ]; then
+if [[ $1 == "fast" ]]; then
         ssh -i ./scripts/deploy_key totem@totem.local "unzip -o totem.zip -d totem"
 else
         ssh -i ./scripts/deploy_key totem@totem.local "sudo rm -rf totem && unzip totem.zip -d totem"
